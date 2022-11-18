20d1eab5888d5b0795e1df34864db1ec6696830b
Prabha-2410 <prabhacomp24@gmail.com>
2022-11-19 02:11:41 +0530

    Program Prac Dict

---
+++ Dictionary Exercise.py
@@ -0,0 +1,5 @@
+# QUESTION: Create a Dictionary and take input from user and return the meaning of the word from the dictionary
+d={"Fish":"Water","Food":"Dosa","Prabha":{"Bird":"Cookie","Gender":"Female"},"Fruit":"Mango"}
+print("select your choice: ")
+a=input()
+print(d[a])
