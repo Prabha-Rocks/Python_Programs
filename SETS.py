a427a7b94214f4c1232633def89571e32ddf9f4b
Prabha-2410 <prabhacomp24@gmail.com>
2022-11-19 11:52:32 +0530

    All Abt Sets

---
+++ SETS.py
@@ -0,0 +1,9 @@
+# Set is a collection of well defined objects
+s=set([6,3,7,4])#Initializing a Set
+print(s)#Printing a set
+print(type(s))# Printing Datatype Set
+s.add(9)#To add an element to a set
+print(s)
+s1= s.intersection({7,6,59})# To Find Intersection bet 2 sets
+print(s.intersection(s,s1))# To Print Intersection Between 2 sets
+print(len(s))#To Print Length of set
